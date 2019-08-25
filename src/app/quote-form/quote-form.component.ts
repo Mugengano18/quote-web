62dad5e9be4919c218fabce916cd3b95a80fa771
Mugengano18 <mugenganoa@gmail.com>
2019-08-25 23:39:31 +0200

    i corrected the indentations in some compnents

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output,EventEmitter,Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -7,19 +7,19 @@ import { Quote } from '../quote';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-newQuote:Quote[]=[
-  {name:"",quote:"",author:"",publishDate:new Date},
-];
-@Output()getQuote=new EventEmitter<Quote[]>();
-
-displayQuote(){
-  this.getQuote.emit(this.newQuote); 
-  console.log(this.newQuote);
-  this.newQuote=[
-    {name:"",quote:"",author:"",publishDate:new Date()},
+  newQuote: Quote[] = [
+    { name: "", quote: "", author: "", publishDate: new Date },
   ];
-}
-  
+  @Output() getQuote = new EventEmitter<Quote[]>();
+
+  displayQuote() {
+    this.getQuote.emit(this.newQuote);
+    console.log(this.newQuote);
+    this.newQuote = [
+      { name: "", quote: "", author: "", publishDate: new Date() },
+    ];
+  }
+
   constructor() { }
 
   ngOnInit() {
